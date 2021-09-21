e716cee89517e971bf163cbef92c399b269398aa
kvin-stphns <kvin.stphns@gmail.com>
2021-09-21 17:08:25 -0400

    Latest commit

---
+++ src/components/iframe.js
@@ -0,0 +1,10 @@
+
+import React from "react"
+
+const iframe = () => {
+  return (
+<iframe src="https://cascadr.co/blogs/cozykev" width="100%" height="100%" position="relative" allowFullScreen></iframe>
+)
+}
+
+export default iframe
---
+++ src/components/iframe3D.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Iframe from 'react-iframe';
+
+const iframe3D = () => {
+return (
+<Iframe src="https://tender-chief-magnosaurus.glitch.me" width="100%" height="900px" allowFullScreen />
+
+)
+}
+
+export default iframe3D
